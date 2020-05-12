33223d77edee35f8222948f36fe9629c81e54308
hemanta212 <sharmahemanta.212@gmail.com>
2020-05-12 16:16:10 +0545

    Adds ekantipur custom scraper

--- link_guru/custom_scrapers/__init__.py
+++ link_guru/custom_scrapers/__init__.py
@@ -1,7 +1,9 @@
 from custom_scrapers.thekathmandupost import get_news as ktmpost
+from custom_scrapers.ekantipur import get_news as ekantipur
 
 custom_scrapers_map = {
-    'kathmandupost.com': ktmpost
+    'kathmandupost.com': ktmpost,
+    'ekantipur.com': ekantipur
 }
 
 
