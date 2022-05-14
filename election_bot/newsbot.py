9daaf9393881bd3b0b758ec2512cb12a782168c5
hemanta212 <sharmahemanta.212@gmail.com>
2022-05-15 00:58:52 +0545

    Add footer

--- election_bot/newsbot.py
+++ election_bot/newsbot.py
@@ -32,8 +32,7 @@ def main():
         Lalitpur=get_lalitpur_votes(),
     ) 
     header = "source: https://election.ekantipur.com\n"
-    footer = """^^contribute:  [Bot code](https://github.com/pykancha/reddit-bots) |  [Api code](https://github.com/pykancha/election-scraper) | [Api url for your personal automation](https://g7te1m.deta.dev/)"""
-    footer = ""
+    footer = """^^contribute:  [Bot code](https://github.com/pykancha/reddit-bots) |  [Api code](https://github.com/pykancha/election-api) | [Api url for your personal automation](https://g7te1m.deta.dev/)"""
     text = ''
     for city, data in city_data_map.items():
         text += gen_msg(city, data) if city!='Kathmandu' else gen_msg(city, data, concat_name=True)
