4e54237191266aa0652d189233e9c4246c571d4e
hemanta212 <sharmahemanta.212@gmail.com>
2022-05-16 15:13:41 +0545

    Update formatting

--- election_bot/newsbot.py
+++ election_bot/newsbot.py
@@ -33,29 +33,39 @@ def main():
     submissions = [reddit.submission('upxo3j')]
     try:
         city_data_map = dict(
-            Kathmandu=get_ktm_votes(),
-            Bharatpur=get_bharatpur_votes(),
-            Hetauda=get_hetauda_votes(),
-            Damak=get_damak_votes(),
-            Janakpur=get_janakpur_votes(),
-            Dhangadi=get_dhangadi_votes(),
-            Pokhara=get_pokhara_votes(),
-            Biratnagar=get_biratnagar_votes(),
-            Lalitpur=get_lalitpur_votes(),
+            Kathmandu=get_ktm_votes,
+            Bharatpur=get_bharatpur_votes,
+            Hetauda=get_hetauda_votes,
+            Damak=get_damak_votes,
+            Janakpur=get_janakpur_votes,
+            Dhangadi=get_dhangadi_votes,
+            Pokhara=get_pokhara_votes,
+            Biratnagar=get_biratnagar_votes,
+            Lalitpur=get_lalitpur_votes,
         )
     except Exception as e:
         print("Scraper error", e)
         time.sleep(10)
         return
 
-    news = "# News of Interest\n- [A Look at Balen's Core team and their strategic planning for KTM mayoral election](https://shilapatra.com/detail/85494) ([Reddit discussion](https://www.reddit.com/r/Nepal/comments/uqe55n/team_balen_4_people_involved_in_his_campaign/))\n\n"
-    footer = """\n\n**Election Data Source**: https://election.ekantipur.com\n\n*contribute*:  [Bot code](https://github.com/pykancha/reddit-bots) |  [Api code](https://github.com/pykancha/election-api) | [Api url for your personal automation](https://g7te1m.deta.dev/)"""
+    source = "\n\n **Election Data Source**: https://election.ekantipur.com"
+    news = ("# News of Interest\n"
+           "- [A Look at Balen's Core team and their strategic planning for KTM mayoral election]"
+                "(https://shilapatra.com/detail/85494)"
+              "([Reddit discussion]"
+                "(https://www.reddit.com/r/Nepal/comments/uqe55n/team_balen_4_people_involved_in_his_campaign/))\n\n\n\n"
+    )
+    footer = ("\n\n *contribute*: "
+             "[Bot code](https://github.com/pykancha/reddit-bots) |"
+             "[Api code](https://github.com/pykancha/election-api) |"
+             "[Api url for your personal automation](https://g7te1m.deta.dev/) "
+    )
     text = ''
     for city, data in city_data_map.items():
         text += gen_msg(city, data) if city!='Kathmandu' else gen_msg(city, data, concat_name=True)
+        time.sleep(2)
 
-    submission_body = f"{text}\n{news}\n\n{footer}"
-
+    submission_body = f"{text}\n\n\n\n{source}\n\n{news}\n\n{footer}"
     for submission in submissions:
         body = submission.selftext if not hasattr(submission, 'body') else submission.body
         if body.strip() == submission_body.strip():
@@ -66,10 +76,16 @@ def main():
 
 
 def gen_msg(city, data, concat_name=False):
-    mayor = f"# {city}\n## Mayor\n"
-    if city != 'Kathmandu' and data.get('total_votes', 0) and data.get('percentage', 0):
-        extra = f"- Vote Counted: {data['percentage']}% ({data['vote_counted']:,} of {data['total_votes']:,})"
-        mayor = f"# {city}\n{extra}\n\n## Mayor\n"
+    data = data()
+    voter_stat = ''
+    if city == 'Kathmandu':
+        voter_stat = (
+                f"- Total eligible Voters: 300,242 (64% = {data['total_votes']:,})\n"
+                 f"- Vote Counted: {data['percentage']}% ({data['vote_counted']:,})"
+        )
+    elif data.get('total_votes', 0) and data.get('percentage', 0):
+        voter_stat = f"- Vote Counted: {data['percentage']}% ({data['vote_counted']:,} of {data['total_votes']:,})"
+    metadata = f"# {city}\n{voter_stat}\n\n"
 
     # Utils functions
     get_name = lambda x: x['candidate-name'] if not concat_name else x['candidate-name'].split(' ')[0]
@@ -79,7 +95,7 @@ def gen_msg(city, data, concat_name=False):
     # Mayor format
     header = "Candidate|Party|Votes|Percentage|\n:--:|:--:|:--:|:--:|\n"
     candidates = [f"{get_name(i)} | {party(i)} | {i['vote-numbers']} | {vote_percent(i)}%" for i in data['mayor']]
-    mayor += header + "\n".join(candidates)
+    mayor = metadata + header + "\n".join(candidates)
 
     # Deputy Format
     deputy = "\n\n## Deputy Mayor\n"
@@ -87,12 +103,7 @@ def gen_msg(city, data, concat_name=False):
     candidates = [f"{i['candidate-name'].split(' ')[0]}|{party(i)}|{i['vote-numbers']}" for i in data['deputy']]
     deputy = deputy + header + "\n".join(candidates) if candidates else ""
 
-    # Vote stats
-    if city == 'Kathmandu':
-        extra = f"**Some Stats**\n\n- Total eligible Voters: 300,242 (64% = {data['total_votes']:,})\n- Vote Counted: {data['percentage']}% ({data['vote_counted']:,})"
-        body = f'{mayor}\n\n{extra}\n\n{deputy}\n\n'
-    else:
-        body = f'{mayor}\n\n{deputy}\n\n' if deputy else f'{mayor}\n\n'
+    body = f'{mayor}\n\n{deputy}\n\n' if deputy else f'{mayor}\n\n'
     return body
 
 if __name__ == "__main__":
