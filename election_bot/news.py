7379d5082f9ef06d6cd5d4f9f166206ab346d2fd
hemanta212 <sharmahemanta.212@gmail.com>
2022-05-22 18:22:16 +0545

    End dharan votes

--- election_bot/news.py
+++ election_bot/news.py
@@ -235,6 +235,7 @@ def get_dharan_votes():
     counted_votes = sum_total(mayor_dict)
     counted_votes += 0.23 * counted_votes
     total_votes = 62_897
+    counted_votes = total_votes
     vote_percentage = round((counted_votes / total_votes) * 100, 2)
 
     return {
